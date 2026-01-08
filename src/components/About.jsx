50d9fce8fee27fcdb872ac097c61f20bf383fbf0
Tahmid Ibne Mofazzol <tahmid.ibne@gmail.com>
2026-01-08 21:01:54 +0600

    animation added in the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,12 @@
 import germanBook from "../assets/GemanLanguageBook.png";
+import "animate.css";
 
 const About = () => {
 	return (
 		<div className="w-[95%] mx-auto mb-15 bg-[#F9FAFB] rounded-2xl">
 			<div className="grid grid-cols-1 lg:grid-cols-2 p-5">
 				<div>
-					<h1 className="text-[30px] mb-3 md:text-[40px] md:mb-6 font-bold w-full md:w-[80%] text-[#3D348B]">Learn German the Smarter Way</h1>
+					<h1 className="text-[30px] mb-3 md:text-[40px] md:mb-6 font-bold w-full md:w-[80%] text-[#3D348B] animate__animated animate__wobble">Learn German the Smarter Way</h1>
 					<h3 className="md:text-[24px] md:mb-4 text-[20px] font-semibold mb-3 text-[#7678ED] ">Build your vocabulary with easy, structured, and interactive lessons.</h3>
 					<p className="text-[#3d348bb0] px-4 mb-4 md:text-[17px]">Our mission is to make learning German vocabulary simple, engaging, and effective. Whether you're starting out or expanding your skills, our platform helps you learn new words through clear pronunciations, real-life examples, and themed lessons. Bite-sized sessions, personalized difficulty, and practical usage tips help you retain vocabulary — one word at a time.</p>
 				</div>
